437cd680f7e01b74acb92e33d6becabc12fc69d0
SalmanHabeeb <salmangithub137@gmail.com>
2023-12-31 14:45:51 +0530

    Add python utils
    *.py

---
+++ export_model.py
@@ -0,0 +1,78 @@
+import struct
+import torch
+from torch import nn
+import argparse
+from model import Mamba
+
+MAX_SEQUENCE_LENGTH = 256
+
+def serialize_fp32(file, tensor):
+    """ writes one fp32 tensor to file that is open in wb mode """
+    d = tensor.detach().cpu().view(-1).to(torch.float32).numpy()
+    b = struct.pack(f'{len(d)}f', *d)
+    file.write(b)
+
+def export_mamba_for_c(model, filepath):
+    """
+    Export the model weights in full float32 .bin file to be read from C.
+    This is same as legacy_export, but with a proper header.
+    """
+    out_file = open(filepath, 'wb')
+
+    # 1) write the params, which will be 7 ints
+    n_layers = model.args.n_layer
+    vocab_size = model.args.vocab_size
+    dim = model.args.d_model
+    d_state = model.args.d_state
+    d_conv = model.args.d_conv
+    expand = model.args.expand
+    dt_rank = model.args.dt_rank
+    max_seq_len = MAX_SEQUENCE_LENGTH
+    header = struct.pack('iiiiiiii', n_layers,
+                         vocab_size, dim, d_state, d_conv, expand, dt_rank,
+                         max_seq_len)
+    out_file.write(header)
+
+    # 2) write some other flags
+    shared_classifier = torch.equal(model.embedding.weight, model.lm_head.weight)
+    out_file.write(struct.pack('B', int(shared_classifier)))
+    pad = 256 - out_file.tell() # pad rest with zeros; tell returns current pos
+    assert pad >= 0
+    out_file.write(b'\0' * pad)
+
+    # 3) now let's write out all the params
+    weights = [
+        model.embedding.weight,
+        *[layer.norm.weight for layer in model.layers],
+        *[layer.mixer.in_proj.weight for layer in model.layers],
+        *[layer.mixer.conv1d.weight for layer in model.layers],
+        *[layer.mixer.conv1d.bias for layer in model.layers],
+        *[layer.mixer.x_proj.weight for layer in model.layers],
+        *[layer.mixer.dt_proj.weight for layer in model.layers],
+        *[layer.mixer.dt_proj.bias for layer in model.layers],
+        *[layer.mixer.out_proj.weight for layer in model.layers],
+        *[layer.mixer.A_log for layer in model.layers],
+        *[layer.mixer.D for layer in model.layers],
+        model.norm_f.weight,
+    ]
+
+    if not shared_classifier:
+        weights.append(model.lm_head.weight)
+
+    for w in weights:
+        serialize_fp32(out_file, w)
+
+    # write to binary file
+    out_file.close()
+    print(f"wrote {filepath}")
+
+def model_export(checkpoint, file_path):
+    model = Mamba.from_pretrained("state-spaces/mamba-130m")
+    export_mamba_for_c(model, file_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", type=str, help="the output filepath")
+    parser.add_argument("--checkpoint", type=str, help="huggingface model", default="state-spaces/mamba-130m")
+    args = parser.parse_args()
+    model_export(args.checkpoint, args.filepath)
---
+++ export_tokenizer.py
@@ -0,0 +1,68 @@
+from transformers import AutoTokenizer
+import argparse
+import os
+import struct
+import tempfile
+
+def get_merge_data(tokenizer):
+   merge_data = None
+   with tempfile.TemporaryDirectory() as tempdir_path:
+       tokenizer.save_vocabulary(tempdir_path)
+
+       with open(os.path.join(tempdir_path, "merges.txt"), "r") as f:
+           # Split by lines, skip first line, which contains version info
+           merge_data = f.read().splitlines()[1:]
+
+   # Remove spaces in merge data, to merge tokens
+   return [to_merge.replace(" ", "") for to_merge in merge_data]
+
+def export_tokenizer_for_c(tokenizer, filepath):
+  merge_data = get_merge_data(tokenizer)
+  tokens, scores = [], []
+  for i in range(tokenizer.vocab_size):
+
+      # decode the token and light postprocessing
+      t = tokenizer._tokenizer.model.id_to_token(i)
+      s = None
+      try:
+        s = tokenizer.vocab_size - merge_data.index(t)
+      except:
+        print(t)
+        s = -1
+      if i == tokenizer.bos_token_id:
+          t = '\n<s>\n'
+      t = t.replace('Ġ', ' ')  # gptneoxtokenizer uses this character as whitespace
+      t = t.replace('č', '\r') # gptneoxtokenizer uses this character as \r
+      t = t.replace('Ĉ', '\b') # gptneoxtokenizer uses this character as \b
+      t = t.replace('Ċ', '\n') # gptneoxtokenizer uses this character as \n
+      t = t.replace('ĉ', '\t') # gptneoxtokenizer uses this character as \t
+
+      b = t.encode('utf-8') # bytes of this token, utf-8 encoded
+
+      tokens.append(b)
+      scores.append(s)
+
+  max_token_length = max(len(t) for t in tokens)
+
+  # # write to a binary file
+  with open(filepath, 'wb') as f:
+      f.write(struct.pack("I", max_token_length))
+      for token_bytes, score in zip(tokens, scores):
+          f.write(struct.pack("fI", score, len(token_bytes)))
+          f.write(token_bytes)
+
+def tokenizer_export(tokenizer_name, chat_template, filepath):
+  tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+  tokenizer.eos_token = "<|endoftext|>"
+  tokenizer.pad_token = tokenizer.eos_token
+  tokenizer.chat_template = AutoTokenizer.from_pretrained(chat_template).chat_template
+  export_tokenizer_for_c(tokenizer, filepath)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("filepath", type=str, help="output file path")
+  parser.add_argument("--tokenizer", type=str, help="Huggingface path to tokenizer", default="EleutherAI/gpt-neox-20b")
+  parser.add_argument("--chat-template", type=str, help="Huggingface path to chat-template", default="HuggingFaceH4/zephyr-7b-beta")
+  args = parser.parse_args()
+  tokenizer_export(args.tokenizer, args.chat_template, args.filepath)
